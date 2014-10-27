76d5c511c045262ae3195707a05b889715b7876e
shukon <shukon@users.noreply.github.com>
2014-10-27 22:48:20 +0100

    Create ConnectFourSolver.cpp

---
+++ ConnectFourSolver.cpp
@@ -0,0 +1,23 @@
+// Copyright 2014, University of Freiburg,
+// Chair of Algorithms and Data Structures.
+// Author: Hannah Bast <bast@cs.uni-freiburg.de>.
+
+#include<stdlib.h>
+
+#include "./ConnectFourMove.h"
+#include "./ConnectFourStateBase.h"
+#include "./ConnectFourSolver.h"
+
+ConnectFourSolver solver;  // objekt
+ConnectFourSolverBase* SOLVER = &solver;  // zeiger
+
+// _____________________________________________________________________________
+ConnectFourMove ConnectFourSolver::generateMove(
+    const ConnectFourStateBase* state) {
+  ConnectFourMove move;
+  // So far only random move as in the example of Prof. Bast
+  move.row = random() % state->numRows();
+  move.col = random() % state->numCols();
+  move.type = ConnectFourMove::TWO;
+  return move;
+}
